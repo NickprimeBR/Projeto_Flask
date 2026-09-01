bf5ec3be20665ce048f85ef383891f6cca9baecb
Nicolas <nickprime2112@gmail.com>
2026-09-01 13:05:09 -0300

    atualização pendente vercell

--- app.py
+++ app.py
@@ -8,12 +8,33 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('dashboard/index.html')
 
 
 @app.route('/sobre')
 def sobre_o_Sistema():
-    return render_template('sobre.html')
+    return render_template('dashboard/sobre.html')
+
+@app.route('/aluno')
+def lista_aluno():
+    lista=[
+       (1,"Ana Beatriz Silva",20,"Teresina"),
+       (2,"Bruno Carvalho Santos",21,"Parnaíba"),
+        (3,"Carlos Eduardo Lima",19,"Picos"),
+        (4,"Daniela Ferreira Costa",22,"Floriano"),
+        (5,"Eduardo Henrique Alves",20,"Teresina"),
+        (6,"Fernanda Oliveira Sousa",21,"Piripiri"),
+        (7,"Gabriel Martins Rocha",23,"Campo Maior"),
+        (8,"Helena Vitória Mendes",19,"Teresina"),
+        (9,"Igor Rodrigues Silva",22,"Bom Jesus"),
+        (10,"Juliana Alves Pereira",20,"Oeiras")
+        ]
+    
+    return render_template('aluno/lista.html', lista=lista)
+
+@app.route('/professor')
+def lista_professor():
+    return render_template('professor/lista.html')
 
 
 @app.route('/ajuda')
